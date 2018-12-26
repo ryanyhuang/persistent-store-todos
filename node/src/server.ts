8154d1ea7e599a03723deccfb7a21b44ab520288
Ryan Huang <ryh002@ucsd.edu>
2018-12-25 18:14:48 -0800

    backend serves on its own

--- node/src/server.ts
+++ node/src/server.ts
@@ -50,18 +50,23 @@ app.post('/db_dispatch', (req, res) => {
 });
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname+'/app/build/index.html'));
-});
+// app.get('*', (req,res) => {
+//     res.sendFile(path.join(__dirname+'/../dist-react/index.html'));
+// });
 
-if (process.env.NODE_ENV === 'production') {
-	app.use('/images', express.static(path.join(__dirname, '..', 'dist-react', 'images'), { maxAge: 31557600000 }));
-	app.use('/libs', express.static(path.join(__dirname, '..', 'dist-react', 'libs'), { maxAge: 31557600000 }));
-	app.use('/static', express.static(path.join(__dirname, '..', 'dist-react', 'static'), { maxAge: 31557600000 }));
-	app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'dist-react', 'index.html')));
-} else {
-	app.get('/:url', (req, res) => (res.redirect('http://localhost:3001/' + req.params.url)));
-}
+app.use('/images', express.static(path.join(__dirname, '..', 'dist-react', 'images'), { maxAge: 31557600000 }));
+app.use('/libs', express.static(path.join(__dirname, '..', 'dist-react', 'libs'), { maxAge: 31557600000 }));
+app.use('/static', express.static(path.join(__dirname, '..', 'dist-react', 'static'), { maxAge: 31557600000 }));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'dist-react', 'index.html')));
+
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use('/images', express.static(path.join(__dirname, '..', 'dist-react', 'images'), { maxAge: 31557600000 }));
+// 	app.use('/libs', express.static(path.join(__dirname, '..', 'dist-react', 'libs'), { maxAge: 31557600000 }));
+// 	app.use('/static', express.static(path.join(__dirname, '..', 'dist-react', 'static'), { maxAge: 31557600000 }));
+// 	app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'dist-react', 'index.html')));
+// } else {
+// 	app.get('/:url', (req, res) => (res.redirect('http://localhost:3001/' + req.params.url)));
+// }
 
 app.use(errorHandler());
 
